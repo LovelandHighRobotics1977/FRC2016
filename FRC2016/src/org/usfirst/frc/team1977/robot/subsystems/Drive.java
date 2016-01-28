34a65d2fdcb3ed6e2f1941a29eee04a6c7b3ead4
Kuragama <evanrs@earthlink.net>
2016-01-28 16:29:23 -0700

    Drive subsystem initial
    Added skeleton classes for the drive subsystem and userdrive command.

---
+++ FRC2016/src/org/usfirst/frc/team1977/robot/subsystems/Drive.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team1977.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Loveland High Robotics 1977
+ */
+public class Drive extends Subsystem {
+	
+	//HIIIIIIIIIIIII
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
