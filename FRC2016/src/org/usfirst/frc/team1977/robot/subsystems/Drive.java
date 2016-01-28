0c2a6d3a35845866807ba5697babc4efb7971a0d
lnelson2382 <lnelson2382@gmail.com>
2016-01-28 16:43:41 -0700

    Partial update of the drive system

--- FRC2016/src/org/usfirst/frc/team1977/robot/subsystems/Drive.java
+++ FRC2016/src/org/usfirst/frc/team1977/robot/subsystems/Drive.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1977.robot.subsystems;
+import org.usfirst.frc.team1977.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Talon;
+//2016
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -8,11 +11,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drive extends Subsystem {
 	
-	//HIIIIIIIIIIIII
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	private static Drive instance;
+	
+	private Talon frontLeft;
+	private Talon frontRight;
+	private Talon backLeft;
+	private Talon backRight;
+	
+	private UserDrive userDrive;
+	
+	private boolean speedToggle = false;
+	private double voltageCoefficient = 1;
+	private double turnPowerCoefficient = 1;
+	
+	public Drive() {
+		frontLeft = new Talon(RobotMap.DRIVE_FRONT_LEFT_TALON);
+		frontRight = new Talon (RobotMap.DRIVE_FRONT_RIGHT_TALON);
+		backLeft = new Talon(RobotMap.DRIVE_LEFT_TALON);
+		backRight = new Talon(RobotMap.DRIVE_BACK_RIGHT_TALON);
+	}
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
