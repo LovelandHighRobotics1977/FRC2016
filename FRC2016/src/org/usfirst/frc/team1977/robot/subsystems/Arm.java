065d3d73eafd9c31ca1e66987c41facc71bb3bfc
LordSneezyCat <sneezcat16@gmail.com>
2016-01-29 17:12:18 -0700

    I started the Arm code.

---
+++ FRC2016/src/org/usfirst/frc/team1977/robot/subsystems/Arm.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team1977.robot.subsystems;
+import org.usfirst.frc.team1977.robot.RobotMap;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ *
+ */
+public class Arm extends Subsystem {
+    public static Arm instance;
+    private Victor ArmA;
+    private Victor ArmB;
+	
+
+    public void initDefaultCommand() {
+        
+    }
+}
+
