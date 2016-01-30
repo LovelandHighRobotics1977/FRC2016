2b0642b52a9cfce3f57b8c797008542eb46b5214
lnelson2382 <lnelson2382@gmail.com>
2016-01-30 14:36:35 -0700

    Fixed last remaining OI compile error

--- FRC2016/src/org/usfirst/frc/team1977/robot/Robot.java
+++ FRC2016/src/org/usfirst/frc/team1977/robot/Robot.java
@@ -33,7 +33,7 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-		oi = new OI();
+		oi = OI.getInstance();
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
